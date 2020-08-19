e1963d60a00046f4a67a35384603022cc76f0964
bornmetal <60639315+bornmetal@users.noreply.github.com>
2020-08-18 21:15:59 -0700

    Add files via upload

---
+++ index (3).php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Ace In The Hole Multi-sports</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Karma:wght@500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0" nonce="1shVGe2r"></script>
+
+
+<!-- top section -->
+<div class="section header">
+    <div class="container">
+        <div class="row">
+            <div class="nav-container">
+                <nav>
+                    <input type="checkbox" id="nav-toggle">
+                    <label for="nav-toggle" class="burger-menu">
+                        <img src="imgs/menu.jpg" alt="menu" width="80" height="80">
+                    </label>
+                    <div class="left-menu">
+                        <a href="index.php">Home</a>
+                        <a href="forms/reservations.html.php">Events</a>
+                        <a href="#">About</a>
+                        <a href="#">multimedia</a>
+                    </div>
+                    <a href="index.php" class="logo">Ace In The Hole Multi-sports</a>
+                </nav>
+            </div>
+
+
+
+
+        </div>
+        <div class="row action">
+            <h1>Wy Not you?</h1>
+            <h2>Multisports for Everyone</h2>
+            <h2><a href="#" class="button button-primary">Register Today!</a></h2>
+        </div>
+    </div>
+</div>
+
+<!-- info section -->
+<div class="section info">
+    <div class="container info">
+        <div class="row">
+            <h2>We Want You!!</h2>
+        </div>
+        <div class="row">
+
+
+            <div class="eight columns">
+                <p>Mei te possit instructior, idque delenit qui et, dicit ludus commune vix ad. Ea modo dicam cetero mel, case disputationi at est. Ad nam adipisci dignissim posidonium, eius reque temporibus ne mea. Vero antiopam ea vim. Ei timeam electram vix. Eam at tollit erroribus, ea eum idque legere epicuri. Sed quot legere minimum cu, ex mei fastidii mandamus intellegam, error consul ut per.</p>
+                <p><a href="#" class="button button-primary">More info</a></p>
+            </div>
+            <div class="two columns">
+                <h4>Social Media</h4>
+                
+                <div class="fb-comment-embed" data-href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" data-include-parent="false" data-width="300"></div>
+                
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <div class="section footer">
+        <div class="container">
+            <div class="row">
+                <?php include 'includes/footer.php';?>
+                
+            </div>
+        </div>
+    
+</div>
+</body>
+</html>
