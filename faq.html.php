508139085508a35d8f1040f997381041611b3da5
bornmetal <60639315+bornmetal@users.noreply.github.com>
2020-08-28 22:21:51 -0700

    Add files via upload

---
+++ faq.html.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Ace In The Hole Multi-sports</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Karma:wght@500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0" nonce="1shVGe2r"></script>
+
+
+<!-- top section -->
+<div class="section header">
+    <div class="container">
+        <div class="row">
+            <div class="nav-container">
+                <nav>
+                    <input type="checkbox" id="nav-toggle">
+                    <label for="nav-toggle" class="burger-menu">
+                        <img src="imgs/menu.jpg" alt="menu" width="80" height="80">
+                    </label>
+                    <div class="left-menu">
+                         <a href="index.php">Home</a>
+                        <a href="forms/">Events</a>
+                        <a href="about/">About</a>
+                        <a href="imggallery/photos.html.php">multimedia</a>
+                        <a href="faq.html.php">FAQ</a>
+                        
+                    </div>
+                    <a href="index.php" class="logo">Ace In The Hole Multi-sports</a>
+                </nav>
+            </div>
+
+
+
+
+        </div>
+        <div class="row action">
+            <h1>Wy Not you?</h1>
+            <h2>Multisports for Everyone</h2>
+            <h2><a href="#" class="button button-primary">Register Today!</a></h2>
+        </div>
+    </div>
+</div>
+
+<!-- info section -->
+<div class="section info">
+    <div class="container info">
+        <div class="row">
+            <h2>We Want You!!</h2>
+        </div>
+        <div class="row">
+
+
+            <div class="eight columns">
+
+
+                <h3>Saturday</h3>
+                <p>  Long Course Triathlon 7:00 AM</p>
+                <P>  Olympic Triathlon 7:30 AM</P>
+                <P>    10K 7:15 AM</P>
+                <P>    Half Marathon 7:15 AM</P>
+
+                <P></P>
+                   <H3> Sunday</H3>
+                <P> Sprint Triathlon 8:00 AM</P>
+                <P>  Try-a-Tri 8:20 AM</P>
+                <P> Splash n Dash 12:00 PM  </P>
+                <P></P>
+
+                   <h3> ABOUT ACE IN THE HOLE MULTISPORT EVENTS</h3>
+                <P></P>
+                    Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds. We offer Events for Every Body.<P></P>
+                <h3>ABOUT THE EVENT</h3>
+                <P></P>
+                    The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.
+
+                    There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!
+                <P></P>
+                <h4>What to Bring</h4>
+                    Watch the weather closely. The show goes on no matter what the weather is doing.
+
+                You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip timing piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, you will have a very difficult time getting off your wet suit.
+                <P></P>
+               <h4> Swim</h4>
+                Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want to bring your own goggles.
+                <P></P>
+               <h4> Bike:</h4>
+                A biking helmet is mandatory. You will also receive 2 stickers in your package with your race number on them. The small sticker should go on the front of your helmet. The bigger sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For safety reasons, be sure to have end-caps on the end of your handle-bars.
+                <P></P>
+                <h4>Run</h4> You must finish the race with your bib number on the front of you. Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don't have to worry about it. Others pin it on a singlet that they put on once they finish the swim before they head out for the bike. Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave for the run. Use whatever option feels best for you.
+
+                Remember to bring a change of clothing so you can enjoy the post-event festivities.
+<p></p>
+
+               <h3> FAQs</h3>
+                <p></p>
+                <h4>What are the Rules? </h4>
+                <p></p>
+                We currently adhere to the USAT Rules for Triathlon & Duathlon. Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike for obvious safety reasons.
+                <p></p>
+               <h4> Can I use a personal music device while cycling? </h4>
+                Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.
+                <p></p>
+               <h4> Can I use a personal music device while running?</h4>
+                During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions. With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate.
+
+                If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice - music or awards/points/rankings.
+                If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don't want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.
+               <p></p>
+                <h4>Do I need to wear a wetsuit?</h4> No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor.
+                <p></p>
+                <h4>Do I have to use a road or racing bike? </h4>No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.
+             <p>   </P>
+
+              <h3>  REGISTRATION</h3>
+                <p></p>
+                <h4>Long Course $240</h4>
+                <p></p>
+                <h4>Olympic $110</h4>
+                <p></p>
+               <h4> 10K $50</h4>
+                  <p></p>
+               <h4> Half Marathon $75</h4>
+                <p></p>
+                <h4>Sprint $90</h4>
+                <p></p>
+
+                <h4>Try-a-Tri $65</h4>
+               <p></p>
+
+                <h3>Cost Includes</h3>
+                <p></p>
+                <ul>
+                    <li>Food & Beer</li>
+                    <li>Access to the weekend's live entertainment & Fitness Expo</li>
+                    <li>Commemorative Finisher medal</li>
+                    <li>Accurate Chip Timing for competitive races</li>
+                    <li>Ace in the Hole Multisport Weekend Tech Shirt</li>
+                    <li>Post-event party & entertainment</li>
+                </ul>
+                <p></p>
+                *NOTE: Tech shirts guaranteed to pre-registered participants only.
+
+
+                <h3>PACKET PICK UP</h3>
+                <p></p>
+                All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR. Packet pick up hours: Thursday 8-5, Friday 9-noon. No day of event packet pick up.
+                <p></p>
+
+                <h3>COURSE DETAILS</h3>
+                <p></p>
+                WATER TEMPERATURE is expected to be between 62 & 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.
+<p></p>
+                Long Course SWIM - 1.2mi - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.
+<p></p>
+                Long Course BIKE - 58 Miles: A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.
+<p></p>
+                Long Course RUN - 13.1mi - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).
+<p></p>
+                OLYMPIC SWIM - 1,500 meters - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.
+<p></p>
+                OLYMPIC BIKE - 28mi - A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.
+<p></p>
+                OLYMPIC RUN - 10K - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).
+<p></p>
+                Sprint: Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.
+<p></p>
+                Try-A-Tri: This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).
+
+
+                <p><a href="#" class="button button-primary">More info</a></p>
+            </div>
+            <div class="two columns">
+                <h4>Social Media</h4>
+                
+                <div class="fb-comment-embed" data-href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" data-include-parent="false" data-width="300"></div>
+                
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <div class="section footer">
+        <div class="container">
+            <div class="row">
+                <?php include 'includes/footer.php';?>
+                
+            </div>
+        </div>
+    
+</div>
+</body>
+</html>
